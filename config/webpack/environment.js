caf36ebef3aba60abb965d38c18d2f83d39b9554
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-02-25 03:17:49 -0300

    First Commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,9 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const customConfig = require('./custom')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.config.merge(customConfig)
+module.exports = environment
